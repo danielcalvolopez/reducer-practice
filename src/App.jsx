1f36e68b54322ce5f994ac5e1bfd734a4aecbd60
Daniel <acidistrict@gmail.com>
2022-11-25 19:47:41 +0000

    add usereducer to change name state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useReducer } from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "SET_NAME":
+          return { ...state, name: action.payload };
+      }
+    },
+    {
+      names: [],
+      name: "",
+    }
+  );
+  return (
+    <div className="App">
+      <input
+        type="text"
+        value={state.name}
+        onChange={(event) =>
+          dispatch({ type: "SET_NAME", payload: event.target.value })
+        }
+      />
+
+      <div>{state.name}</div>
+    </div>
+  );
 };
 
 export default App;
