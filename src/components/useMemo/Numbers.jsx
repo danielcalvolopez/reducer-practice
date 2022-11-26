d05f9928b42cca660ea35e852ae317a3b3ffa7e0
Daniel <acidistrict@gmail.com>
2022-11-26 11:40:05 +0000

    usememo on sorted function

--- src/components/useMemo/Numbers.jsx
+++ src/components/useMemo/Numbers.jsx
@@ -1,14 +1,29 @@
 import { useState, useMemo } from "react";
 
 const Numbers = () => {
-  const [numbers, setNumbers] = useState([10, 20, 30]);
+  const [numbers] = useState([10, 20, 30]);
 
   const total = useMemo(
     () => numbers.reduce((accumulator, number) => accumulator + number, 0),
     [numbers]
   );
 
-  return <div>Total: {total}</div>;
+  const [names] = useState(["Mike", "Jack", "Max", "Marcos"]);
+
+  const sortedNames = useMemo(() => [...names].sort(), [names]);
+
+  return (
+    <div>
+      <div>Total: {total}</div>
+      <div>{names.join(", ")}</div>
+      <div>
+        Names:
+        {sortedNames.map((name, index) => (
+          <p key={index}>{name}</p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Numbers;
