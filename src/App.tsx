b629b08456f094c471cf242cd325a4eb5b61ca78
Daniel <acidistrict@gmail.com>
2022-12-05 19:40:38 +0000

    create a counter and passing state props typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Provider } from "react-redux";
 import "./App.css";
+import Counter from "./components/ typescript/Counter";
 import Heading from "./components/ typescript/Heading";
 import Section from "./components/ typescript/Section";
 import { PokemonContextProvider } from "./components/useContext/PokemonContext";
@@ -8,12 +9,14 @@ import { PokemonContextProvider } from "./components/useContext/PokemonContext";
 import { store } from "./redux/store/store";
 
 const App = () => {
+  const [count, setCount] = useState<number>(0);
   return (
     <Provider store={store}>
       <PokemonContextProvider>
         <div className="App">
           <Heading title={"Hello"} />
           <Section>This is the children</Section>
+          <Counter setCount={setCount}>Count is {count}</Counter>
         </div>
       </PokemonContextProvider>
     </Provider>
