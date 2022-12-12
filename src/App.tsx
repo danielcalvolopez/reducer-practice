26601f04a4b2b04bdef7b3d98d7f5434725291a4
Daniel <acidistrict@gmail.com>
2022-12-12 19:33:09 +0000

    setting up kanban project

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
-import React, { useState } from "react";
-import { Provider } from "react-redux";
-import "./App.css";
-import Counter from "./components/ typescript/Counter";
-import Footer from "./components/ typescript/Footer";
-import Heading from "./components/ typescript/Heading";
-import List from "./components/ typescript/List";
-import Section from "./components/ typescript/Section";
-import { PokemonContextProvider } from "./components/useContext/PokemonContext";
+import React from "react";
 
-import { store } from "./redux/store/store";
+import "./App.css";
 
 const App = () => {
-  const [count, setCount] = useState<number>(0);
-  return (
-    <Provider store={store}>
-      <PokemonContextProvider>
-        <div className="App">
-          <Heading title={"Hello"} />
-          <Section>This is the children</Section>
-          <Counter setCount={setCount}>Count is {count}</Counter>
-          <List
-            items={["Spain", "Emgland", "Brazil"]}
-            render={(item: string) => <span>{item}</span>}
-          />
-          <Footer />
-        </div>
-      </PokemonContextProvider>
-    </Provider>
-  );
+  return <div className="App">hey</div>;
 };
 
 export default App;
